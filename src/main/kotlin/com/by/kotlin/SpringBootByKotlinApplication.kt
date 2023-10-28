e4964c5cfe585c918fcac5f5c812a44a4a33568f
DongHoKim <cha11enge@hanmail.net>
2023-10-28 15:58:18 +0900

    [SET] Create Project

---
+++ src/main/kotlin/com/by/kotlin/SpringBootByKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.by.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootByKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootByKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/by/kotlin/SpringBootByKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.by.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootByKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
