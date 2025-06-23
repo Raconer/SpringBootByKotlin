198a81a6900f9eb499bf2d57bebff0d2fb874cb2
kimdongho <cha11enge@hanmail.net>
2025-06-23 17:57:03 +0900

    [KAFKA] 설정

--- src/main/kotlin/com/by/kotlin/common/mq/kafka/KafkaConsumer.kt
+++ src/main/kotlin/com/by/kotlin/common/mq/kafka/KafkaConsumer.kt
@@ -9,13 +9,15 @@ import org.springframework.stereotype.Service
 @Service
 class KafkaConsumer {
 
+    var MSG_REQ_CNT = 0
+
     @KafkaListener(
         topics = ["kafka-topic"],
         groupId = "kafka-group-id"
     )
     fun listen(@Payload message: String, acknowledgment: Acknowledgment) {
         try {
-            println("\uD83D\uDCE5 수신 메시지: $message")
+            println("\uD83D\uDCE5 수신 메시지(${MSG_REQ_CNT++}): $message")
             acknowledgment.acknowledge()
         } catch (e: Exception) {
             println("❌ 예외 발생: ${e.message}")
--- src/main/kotlin/com/by/kotlin/common/mq/kafka/KafkaProducer.kt
+++ src/main/kotlin/com/by/kotlin/common/mq/kafka/KafkaProducer.kt
@@ -8,13 +8,16 @@ import org.springframework.stereotype.Service
 class KafkaProducer (
     private val kafkaTemplate: KafkaTemplate<String, String>
 ){
+
+    var MSG_REQ_CNT = 0
+
     fun sendMessage(message: String) {
         val future = this.kafkaTemplate.send("kafka-topic", message)
         future.whenComplete { result, ex ->
             if (ex != null) {
                 println("❌ 전송 실패: ${ex.message}")
             } else {
-                println("\uD83D\uDCE4 전송 성공: ${result?.producerRecord}")
+                println("\uD83D\uDCE4 전송 성공[${MSG_REQ_CNT++}]: ${result?.producerRecord}")
             }
         }
     }
